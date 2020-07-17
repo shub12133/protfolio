b8fba8139baf75c171e908cee783cfdfbc6a5130
shub12133 <shub.123433@gmail.com>
2020-07-17 22:35:45 +0530

    image fixed

--- src/yourdata.js
+++ src/yourdata.js
@@ -18,7 +18,7 @@ export default
         // Add Your About Text Here
         aboutText:"Hi, my name is Shubham Sharma. I'm a software engineer based in Ahmedabad, Gujarat. I am a problem solver and a true globalist. I specialized in MERN stack along with Python, and SQL database. Check out my work below and get in touch!",
        //Change This To Hide The Image of About Section (True Or False)
-   aboutimage:"",
+   aboutimage:"https://i.imgur.com/3t48imh.jpg",
        ShowAboutImage:true,//true or false (Change Here)
        // Change Projects Here 
        Resume:
@@ -35,7 +35,7 @@ export default
             title:'Find Band ', //Project Title - Add Your Project Title Here
              service:'React Website Development', // Add Your Service Type Here
             //Project Image - Add Your Project Image Here
-              imageSrc:"https://i.imgur.com/mWR2m5p.png",            
+              imageSrc:"https://i.imgur.com/DItRuJK.jpg",            
                //Project URL - Add Your Project Url Here
              url:'https://bandes.herokuapp.com//',
              githuburl:"https://github.com/shub12133/band"
@@ -44,7 +44,7 @@ export default
                 id: 2,//DO NOT CHANGE THIS (Please)😅
                 title: 'developer contact',
                 service: 'React Website Development',
-                imageSrc:"https://lh3.googleusercontent.com/yXQr0eTF5MZtIFThMt_1RIFxdq2HDaXcTq_qR2AuOfr_DTwDVyzM-6o5IK66dVuUXkGOmPJbNbKums65Oqsky6FAAJ2-bL3TrXQ8cgIVje3Bq6tOydj4cYLUHeK0KuFgvDtwgM1l8orVRe8moiwKB6NK-YirMnWHJPlEfyXP4E1D6mxM5BOIlx6NpHHF2zfE1WuspejeWJiXX5DMWMQycHnz6xBFp5VS9dZ7YsuBS_wqAOO2-ZhSdvAuQJ5R4Zxrvd-k6sDAXBNSsG68kgnv5UsB9RBD8dHursZUnios7noT_Sk_WVjw8qPTPjPWVYXFjkdrNoHoY1A9vU8frcU9vTWQYWu7Pyl93T8DgwaMuz80dfp5WEkNItyeGGgt9cmWJdFRm7OsDiazsaTujO1-tYu_3uYxhtMUgc9LODsu6mDjqoceHmOHJl2ENx_QDywe_QP2sYSFzdC0rIfx2DKfyOgN8aVVTvu7bDxVyqmo-1z1V__MSeMvtq_jckR4hT7RWYbIL5XCPpIxZftq8HyZrr2Ofbnep-HyPJ4KyJ-yyQCE45hVJ1LF1ZV8JS_LEV5lBsRgDvhzB-77io9aNhsdFtviUsjmRAOfXdO_IVa_8FJPgDeNBHDgWT4-2uFErbrHfRQF75fUq4x04HUt0BdkvIy2zuKkdO8W2YbXs9S4D_0Z13iUzvoPlo03JXPN5Q=w761-h1643-no?authuser=0",
+                imageSrc:"https://i.imgur.com/W94WPTD.jpg",            
                 url: 'https://deveeee.herokuapp.com//',
                 githuburl:"https://github.com/shub12133/devconnect",
             },
@@ -75,7 +75,7 @@ export default
             },
             {
             name: 'Resume',
-            url: 'https://drive.google.com/file/d/1EZPOF3EiZN_edtOvHJh-9NOqmYdtb2-k/view?usp=sharing'
+            url: 'https://drive.google.com/file/d/1w6m7rIaiUXJdW3nnv_BPGs--0ZAQDcmT/view?usp=sharing'
         },
 
 
