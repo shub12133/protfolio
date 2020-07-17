27324ad9292f4a970a6bff2bef9a30fe22d1da8d
shub12133 <shub.123433@gmail.com>
2020-07-17 13:44:36 +0530

    the first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+
+import Header from './components/Header';
+import About from './components/About';
+import Work from './components/work';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar'
+// import Resume from './components/Resume';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <div className="App-header">
+      <Header></Header>
+      </div>
+      
+      <div className='About'>
+        <About></About>
+      </div>
+      <div className='work'>
+        <Work></Work>
+      </div>
+      <div className='contact'>
+        <Contact></Contact>
+        </div>
+
+        </div>
+
+
+  );
+}
+
+export default App;
---
+++ src/yourdata.js
@@ -0,0 +1,83 @@
+export default
+    {
+        //(Please Do Not Remove The comma(,) after every variable)
+        //Change The Website Template
+        name :'Shubham Sharma.',
+        headerTagline: [//Line 1 For Header
+                        'Hi 👋 You found me! I am  Shubham Sharma.',
+                        //Line 2 For Header
+                        ' Mern Stack Developer',
+                        //Line 3 For Header
+    ],
+
+    // Resume
+
+    
+        //Contact Email
+        contactEmail:'shub.123433@gmail.com',
+        // Add Your About Text Here
+        abouttext: "Hi, my name is Shubham Sharma. I'm a software engineer based in Ahmedabad, gujrat. I am a problem solver and a true globalist.  I recently transitioned to software development.  I specialized in MERN stack along with react,node js,express and SQL database. Check out my work below and get in touch!",
+        aboutImage:"https://lh3.googleusercontent.com/CYWRdQkuQsl4AaGF-H3479n-R_wtXSat3Zun6dEJZhXAByGDDJ7hjcJUTcJXqZpryfUzrmqdNY8Gzflh0pf_aa7gxd9VB5VkdOt1SK2jp6GOhowImuAUPjtVVZc1YBDLGvemB9eEToPkfmTfp2gLJ4V3sTOzpEmKAVuYo_QwBo_5l9gk4TAr0PdvB7g2e_Oz9LOEPw7GzG-Fj1eb9Arn9KHXBCx96LfNYr0nHvDM8iUohqcEpp2J9ZKpkoP_GfvdO8rUVN5vDNZ4JjAuJemffEedQga7wWdC6NxJqWT7wQklUyVg-wnxLiE1npoK5WYgh7tIZq5edd8ZQo3SSnnNHNOcUc9tIo4bevl704x2zV9cUtbQRFaW0-HxhkVlbphcAqq4wRT2_UzN2Lgz8riKdYZtjVvTC2xYEuD-DJmKzkZ-ZA0B7DJDE_TR_QmV9k4JBrvn6U5XctqeGMolD7koDIItW8GrdGM4y5f4jnOPKX8ns0dLKQCNGMmWEid5tBfdqvPhirYgZyP9Ol3RhH-qT4p7k0EtvSNimF7oEqeyuiazoKud-SUfx7VZnDaILacKiWKQTyWkf2AlE-URIejjX1HZ5UCeJ21EbkIUFprWMgFxcbhA_NVGIox8YGCeTywk4OAlP17SQb6yq89qJGcPtO6Pvm2GpRmVL_s80qTLRNJuhCqJzpjCf6F-vu4JSg=w1158-h1544-no?authuser=0",
+       //Change This To Hide The Image of About Section (True Or False)
+       ShowAboutImage:true,//true or false (Change Here)
+       // Change Projects Here 
+       Resume:
+        {
+        id: 1,//DO NOT CHANGE THIS (Please)😅,
+        title: 'Resume',
+        url:'https://www.slideshare.net/shubhamsharma2403/shubham-sharmacv-2',
+
+        },
+
+       projects:[
+           {
+            id: 1,//DO NOT CHANGE THIS (Please)😅
+            title:'Find Band ', //Project Title - Add Your Project Title Here
+             service:'React Website Development', // Add Your Service Type Here
+            //Project Image - Add Your Project Image Here
+            imgSrc:"https://lh3.googleusercontent.com/UFiDLdkRcf1vaIDMELmTHIYwGOYCj-M_FDwlO0tMUIeHYE6-ayjGR53zPH6RxrVaTwakfBbvBP1na4OdVC-YJkbBVtanSHSD0Sx9XMTiOxZ547FZj-q7Ke5kKbc7j-k-V-leYQF_jDkBoh82GPFQ6RjQOs2jsUB3ClRDCmG-svy1qXzwgMP9DXxAkAMIKObGP36Rlc7XXFYkZltJF91adWSh8OPxoDMV06uEG0OYSEnNqfGBFDjJro85FyXVcuWX2chvK1b3cF2DWVpLxnPMucyAnB9aZpZ71ilDVyMSgJstShdjBuSD96SkXS0UCkb490hOVjhBb0e79FvuXd8QgzXUaR_YXfPYjXN3LryefcdIrxLcXMvGshhybd9v0I6uA9wK1EME0hXC-OPBBYhwXlG00ugmu1sVnkI3sy-P8V-h3B78tuYYcLb7GWBEyZkCD1LpUcmIHsmpNvz9dxjZQ0m3FNPgG7xN_ObdGVvR9agcAl2r9WCda68zDLkXNH76jRAmJuIm6tTR0YbziOX_OnFvxx3ErpvAQYATwJd4ePFMgQDqhNaARRURIbWu-QnafTqhYqjbSkxqxsw3EOgIQgB-VEPUkrIwY_XqIbkDnCvrewEeA8OTOwyZ_IrSrnUhso0b8bDbAoj4Lod1hMYc51cJL5rUI7X8vqb3AnECW5N-DxtnI046CUuOo2htTQ=w739-h1642-no?authuser=0",
+             //Project URL - Add Your Project Url Here
+             url:'https://bandes.herokuapp.com//',
+             githuburl:"https://github.com/shub12133/band"
+            },
+            {
+                id: 2,//DO NOT CHANGE THIS (Please)😅
+                title: 'developer contact',
+                service: 'React Website Development',
+                imageSrc:"https://lh3.googleusercontent.com/yXQr0eTF5MZtIFThMt_1RIFxdq2HDaXcTq_qR2AuOfr_DTwDVyzM-6o5IK66dVuUXkGOmPJbNbKums65Oqsky6FAAJ2-bL3TrXQ8cgIVje3Bq6tOydj4cYLUHeK0KuFgvDtwgM1l8orVRe8moiwKB6NK-YirMnWHJPlEfyXP4E1D6mxM5BOIlx6NpHHF2zfE1WuspejeWJiXX5DMWMQycHnz6xBFp5VS9dZ7YsuBS_wqAOO2-ZhSdvAuQJ5R4Zxrvd-k6sDAXBNSsG68kgnv5UsB9RBD8dHursZUnios7noT_Sk_WVjw8qPTPjPWVYXFjkdrNoHoY1A9vU8frcU9vTWQYWu7Pyl93T8DgwaMuz80dfp5WEkNItyeGGgt9cmWJdFRm7OsDiazsaTujO1-tYu_3uYxhtMUgc9LODsu6mDjqoceHmOHJl2ENx_QDywe_QP2sYSFzdC0rIfx2DKfyOgN8aVVTvu7bDxVyqmo-1z1V__MSeMvtq_jckR4hT7RWYbIL5XCPpIxZftq8HyZrr2Ofbnep-HyPJ4KyJ-yyQCE45hVJ1LF1ZV8JS_LEV5lBsRgDvhzB-77io9aNhsdFtviUsjmRAOfXdO_IVa_8FJPgDeNBHDgWT4-2uFErbrHfRQF75fUq4x04HUt0BdkvIy2zuKkdO8W2YbXs9S4D_0Z13iUzvoPlo03JXPN5Q=w761-h1643-no?authuser=0",
+                url: 'https://deveeee.herokuapp.com//',
+                githuburl:"https://github.com/shub12133/devconnect",
+            },
+            
+                    /*
+
+                    If You Want To Add More Project just Copy and Paste This At The End (Update the id Respectively)
+                ,{
+                id: 5,
+                title: 'Project Five',
+                service: 'Something Amazing',
+                imageSrc: "",
+                url: ''
+            }
+                */
+        ],
+        social: [
+            // Add Or Remove The Link Accordingly
+            {   name:'Github',
+                url:'https://github.com/shub12133'},
+            {
+                name: 'linkedin',
+                url: 'https://www.linkedin.com/in/shubham-sharma-b89203104/'
+            },
+            {
+                name: 'twitter',
+                url: 'https://twitter.com/shub12133'
+            },
+            {
+            name: 'Resume',
+            url: 'https://drive.google.com/file/d/1EZPOF3EiZN_edtOvHJh-9NOqmYdtb2-k/view?usp=sharing'
+        },
+
+
+        ]
+    }
