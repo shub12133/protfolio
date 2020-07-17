82104cbefbade166e15d98db2a0ed33da14a059b
shub12133 <shub.123433@gmail.com>
2020-07-17 19:29:29 +0530

    Updates

---
+++ precache-manifest.b17b5bfa43b0284c10f23b59fc5f20b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c84e438052aa29c462c9695a972d814c",
+    "url": "/https://github.com/git@github.com:shub12133/protfoli.git/index.html"
+  },
+  {
+    "revision": "6db95994402b4059faab",
+    "url": "/https://github.com/git@github.com:shub12133/protfoli.git/static/css/main.74fdb1f7.chunk.css"
+  },
+  {
+    "revision": "c6e53709cc33f90d21d0",
+    "url": "/https://github.com/git@github.com:shub12133/protfoli.git/static/js/2.bcd7bbff.chunk.js"
+  },
+  {
+    "revision": "6db95994402b4059faab",
+    "url": "/https://github.com/git@github.com:shub12133/protfoli.git/static/js/main.d2f340c8.chunk.js"
+  },
+  {
+    "revision": "7a84a8dddc5b7aec9aa3",
+    "url": "/https://github.com/git@github.com:shub12133/protfoli.git/static/js/runtime~main.3c42cfa8.js"
+  }
+]);
