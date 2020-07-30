c0f32d5caec25c000474901c0008f22c39dff93e
shub12133 <shub.123433@gmail.com>
2020-07-30 20:51:05 +0530

    bugs

--- src/yourdata.js
+++ src/yourdata.js
@@ -45,7 +45,7 @@ export default
                 id: 3,//DO NOT CHANGE THIS (Please)😅
                 title: 'Event Calendar',
                 service: 'React Website Development',
-                imageSrc: "https://imgur.com/a/CfLryG5",            
+                imageSrc: "https://imgur.com/a/CfLryG5.jpg",            
                 url: 'https://eventcal111.herokuapp.com/'
             },
             
